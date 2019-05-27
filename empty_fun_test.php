8ca7a6fb4626f010710e4dddb212d0de41caff7b
Sherlock-L <lsw_work@yeah.net>
2019-05-27 18:18:06 +0800

    判空测试

--- empty_fun_test.php
+++ empty_fun_test.php
@@ -2,7 +2,7 @@
 
 
 /**
- * @description 用于不同版本  测试empty, 在5.6 、7.1版本中 empty 认为 0 "0"都是空，都满足empty() 为true 的条件
+ * @description 在5.6 、7.1版本中 的空判断  php,  empty 认为 0 "0"都是空，都满足empty() 为true 的条件
  */
 
 function test()
@@ -12,6 +12,36 @@ function test()
         echo '$a = 0  满足条件 if($a == "") <br>';
     } else {
         echo '$a = 0  不满足条件 if($a == "") <br>';
+    }
+
+    if ($a === "") {
+        echo '$a = 0  满足条件 if($a === "") <br>';
+    } else {
+        echo '$a = 0  不满足条件 if($a === "") <br>';
+    }
+
+    if ($a == "0") {
+        echo '$a = 0  满足条件 if($a == "0") <br>';
+    } else {
+        echo '$a = 0  不满足条件 if($a == "0") <br>';
+    }
+
+    if ($a === "0") {
+        echo '$a = 0  满足条件 if($a === "0") <br>';
+    } else {
+        echo '$a = 0  不满足条件 if($a === "0") <br>';
+    }
+
+    if ($a == null) {
+        echo '$a = 0  满足条件 if($a == null) <br>';
+    } else {
+        echo '$a = 0  不满足条件 if($a == null) <br>';
+    }
+
+    if ($a === null) {
+        echo '$a = 0  满足条件 if($a === null) <br>';
+    } else {
+        echo '$a = 0  不满足条件 if($a === null) <br>';
     }
 
     if ($a) {
@@ -33,6 +63,36 @@ function test()
     } else {
         echo '$a = "0" 不满足条件if($a == "") <br>';
     }
+    
+    if ($a === "") {
+        echo '$a = "0"  满足条件 if($a === "") <br>';
+    } else {
+        echo '$a = "0"  不满足条件 if($a === "") <br>';
+    }
+
+    if ($a == 0) {
+        echo '$a = "0"  满足条件 if($a == 0) <br>';
+    } else {
+        echo '$a = "0"  不满足条件 if($a == 0) <br>';
+    }
+
+    if ($a === 0) {
+        echo '$a = "0"  满足条件 if($a === 0) <br>';
+    } else {
+        echo '$a = "0"  不满足条件 if($a === 0) <br>';
+    }
+
+    if ($a == null) {
+        echo '$a = "0"  满足条件 if($a == null) <br>';
+    } else {
+        echo '$a = "0"  不满足条件 if($a == null) <br>';
+    }
+
+    if ($a === null) {
+        echo '$a = "0"  满足条件 if($a === null) <br>';
+    } else {
+        echo '$a = "0"  不满足条件 if($a === null) <br>';
+    }
 
     if ($a) {
         echo '$a = "0" 满足条件 if($a) <br>';
@@ -51,26 +111,49 @@ function test()
 test();
 
 
+
 /**
- * @description   7.1 version
+ *@description   7.1 version
  * 测试结果：
- * $a = 0       满足条件 if($a == "")
- * $a = 0       不满足条件 if($a)
- * $a = 0       满足条件 if(empty($a))
- * $a = "0"     不满足条件if($a == "")
- * $a = "0"     不满足条件 if($a)
- * $a = "0"     满足条件 if(empty($a))
- */
+    $a = 0 满足条件 if($a == "") 
+    $a = 0 不满足条件 if($a === "") 
+    $a = 0 满足条件 if($a == "0") 
+    $a = 0 不满足条件 if($a === "0") 
+    $a = 0 满足条件 if($a == null) 
+    $a = 0 不满足条件 if($a === null) 
+    $a = 0 不满足条件 if($a) 
+    $a = 0 满足条件 if(empty($a)) 
+
+    $a = "0" 不满足条件if($a == "") 
+    $a = "0" 不满足条件 if($a === "") 
+    $a = "0" 满足条件 if($a == 0) 
+    $a = "0" 不满足条件 if($a === 0) 
+    $a = "0" 不满足条件 if($a == null) 
+    $a = "0" 不满足条件 if($a === null) 
+    $a = "0" 不满足条件 if($a) 
+    $a = "0" 满足条件 if(empty($a)) 
+**/
 
 /**
  * @description   5.6 version
  * 测试结果：
- * $a = 0      满足条件 if($a == "")
- * $a = 0      不满足条件 if($a)
- * $a = 0      满足条件 if(empty($a))
- * $a = "0"    不满足条件if($a == "")
- * $a = "0"    不满足条件 if($a)
- * $a = "0"    满足条件 if(empty($a))
+    $a = 0 满足条件 if($a == "") 
+    $a = 0 不满足条件 if($a === "") 
+    $a = 0 满足条件 if($a == "0") 
+    $a = 0 不满足条件 if($a === "0") 
+    $a = 0 满足条件 if($a == null) 
+    $a = 0 不满足条件 if($a === null) 
+    $a = 0 不满足条件 if($a) 
+    $a = 0 满足条件 if(empty($a)) 
+
+    $a = "0" 不满足条件if($a == "") 
+    $a = "0" 不满足条件 if($a === "") 
+    $a = "0" 满足条件 if($a == 0) 
+    $a = "0" 不满足条件 if($a === 0) 
+    $a = "0" 不满足条件 if($a == null) 
+    $a = "0" 不满足条件 if($a === null) 
+    $a = "0" 不满足条件 if($a) 
+    $a = "0" 满足条件 if(empty($a)) 
  */
 
 
